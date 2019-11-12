3bfaa1574e391496b131c148f3870142fe58768a
Jordi Andreu <jandreu@cells.es>
2019-11-12 11:15:22 +0100

    Clean code

--- HardwareObjects/ALBA/ALBADigitalZoom.py
+++ HardwareObjects/ALBA/ALBADigitalZoom.py
@@ -18,31 +18,30 @@
 #  along with MXCuBE.  If not, see <http://www.gnu.org/licenses/>.
 
 """
-[Name]
 ALBADigitalZoom
 
-[Description]
-Hardware Object used to integrate the zoom capabilities of the Zoom system from
+Hardware Object used to integrate the zoom capabilities of the Bzoom system from
 ARINAX using the TANGO layer.
 
-[Emitted signals]
+Signals:
 - stateChanged
 - predefinedPositionChanged
-"""
 
+"""
 from __future__ import print_function
-from enum import IntEnum, unique
 import logging
 
-from HardwareRepository import BaseHardwareObjects
+from enum import IntEnum, unique
+
+from HardwareRepository.BaseHardwareObjects import Device
 from taurus.core.tango.enums import DevState
 
 __credits__ = ["ALBA"]
-__version__ = "2.3."
+__licence__ = "LGPLv3+"
+__version__ = "2.3"
 __category__ = "General"
 
 
-# As in AbstractMotor class
 @unique
 class DigitalZoomState(IntEnum):
     """
@@ -56,12 +55,14 @@ class DigitalZoomState(IntEnum):
     FAULT = -1
 
 
-class ALBADigitalZoom(BaseHardwareObjects.Device):
+class ALBADigitalZoom(Device):
+    """Hardware object used to control the zoom capabilities."""
 
     STATE = DigitalZoomState
 
     def __init__(self, name):
-        BaseHardwareObjects.Device.__init__(self, name)
+        """ Define variables """
+        Device.__init__(self, name)
         self.logger = logging.getLogger("HWR.ALBADigitalZoom")
         self.chan_pos = None
         self.chan_state = None
@@ -72,14 +73,15 @@ class ALBADigitalZoom(BaseHardwareObjects.Device):
         self.current_state = None
 
     def init(self):
+        """ Initialize variables """
         self.logger.debug("Initializing {0}".format(self.__class__.__name__))
         # TODO: This is not an IOR anymore
         self.chan_pos = self.getChannelObject("position")
         self.chan_state = self.getChannelObject("state")
         # TODO: labels must be harcoded or read as a property
-        #self.chan_labels = self.getChannelObject("labels")
+        # self.chan_labels = self.getChannelObject("labels")
         # TODO: This has to be calibrated with zoom values in the [0,1] range
-        #self.chan_blight = self.getChannelObject('blight')
+        # self.chan_blight = self.getChannelObject('blight')
 
         self.chan_pos.connectSignal("update", self.positionChanged)
         self.chan_state.connectSignal("update", self.stateChanged)
@@ -89,48 +91,50 @@ class ALBADigitalZoom(BaseHardwareObjects.Device):
 
     def getPredefinedPositionsList(self):
         """
-        :return: list of strings (['1 z1', '2 z2', ..., 'n zn']
-        """
-        # labels = self.chan_labels.getValue()
-        # labels = labels.split()
-        # retlist = []
-        # for label in labels:
-        #     pos = str(label.replace(":", " "))
-        #     retlist.append(pos)
-        # # self.logger.debug("Raw zoom-auto positions list: %s" % repr(retlist))
-        # new_retlist = []
-        # for n, e in enumerate(retlist):
-        #     name = e.split()
-        #     new_retlist.append("%s %s" % (n + 1, name[0]))
-        # self.logger.debug("Zoom-auto positions list: %s" % repr(new_retlist))
-        # return new_retlist
-        # TODO: should return list of strings
-        return list(range(1, 8))
+        It returns the corresponding to the zoom positions. In our case,
+        ['1', '2', ..., 'n']. This values should be gathered from the Arinax server
 
-    def moveToPosition(self, posno):
+        :return: [str,]
+
         """
-        Move to one of the predefined positions named posno 'n zn'
-        :param posno:
-        :return:
+        return [str(i) for i in range(1, 8)]
+
+    def moveToPosition(self, position):
         """
-        #no = posno.split()[0]
-        self.logger.debug("Moving to position %s" % posno)
-        self.chan_pos.setValue(int(posno))
+        Move to one of the predefined positions defined in the predefined positions
+        list. In this case, position names correspont to zoom values and only a
+        casting to int is required.
+
+        :param position: string value indicating the zoom position.
+        :return: None
+
+        """
+        # self.logger.debug("Moving to position %s" % position)
+        self.chan_pos.setValue(int(position))
 
     def getLimits(self):
-        # TODO: this should depend on the current configuration
-        # In a digital zoom, this can only be the indexes (no real position)
+        """
+        Get zoom limits (i.e. the position name corresponding to the maximum and
+        minimum values).
+
+        Returns: str, str
+
+        """
         _min = self.getPredefinedPositionsList()[0]
         _max = self.getPredefinedPositionsList()[-1]
         return _min, _max
 
     def getState(self):
         """
-        Digital Zoom state (enum) mapping from current Tango state
+        Get the Digital Zoom state (enum) mapping from current Tango state.
+
         Returns: DigitalZoomState
 
-        """
+        # TODO: servers returns always UNKNWON
         # TODO: Review type and values returned by Arinax server
+
+        """
+
         state = self.chan_state.getValue()
         curr_pos = self.getPosition()
         if state == DevState.ON:
@@ -140,51 +144,82 @@ class ALBADigitalZoom(BaseHardwareObjects.Device):
         elif curr_pos == self.getLimits()[-1]:
             return self.STATE.HIGH_LIMIT
         if state == DevState.UNKNOWN:
-            # TODO: servers returns always UNKNWON
+            # TODO: Bug in Arinax server: always in UNKNOWN
             return self.STATE.READY
-            #return self.STATE.UNKNOWN
+            # return self.STATE.UNKNOWN
         else:
             return self.STATE.FAULT
 
     def getPosition(self):
-        # not a real position, but the index
+        """
+        Get the position index from the server.
+
+        Returns: int
+
+        """
         return self.chan_pos.getValue()
 
     def getCurrentPositionName(self):
-        try:
-            n = int(self.chan_pos.getValue())
-            value = "%s z%s" % (n, n)
-            return str(n)
-            return value
-        except Exception as e:
-            self.logger.debug("Cannot get zoom (<value> z<value>) pair \n%s" % str(e))
-            return None
+        """
+        Returns the current zoom position name.
+
+        Returns: str
+
+        """
+        return str(self.chan_pos.getValue())
 
     def stateChanged(self, state):
-        # TODO: Review state type emitted
+        """
+        Slot receiving the zoom state changed and emitting the signal
+
+        Args:
+            state: Zoom Tango State
+
+        Returns: None
+
+        """
+        # TODO: Arrives a Tango State, emits DigitalZoom State
         # We should use the state received!
-        the_state = self.getState()
-        if the_state != self.current_state:
+        state = self.getState()
+
+        if state != self.current_state:
             self.logger.debug(
-                "old state = %s, new state = %s" % (self.current_state, the_state))
-            self.logger.debug("stateChanged emitted: %s" % the_state)
-            self.current_state = the_state
-            self.emit('stateChanged', (the_state,))
+                "State changed: {} -> {})".format(self.current_state, state))
+            self.current_state = state
+            self.emit('stateChanged', (state,))
 
     def positionChanged(self, position):
-        the_position = self.getCurrentPositionName()
-        if the_position != self.current_position:
-            self.logger.debug("old position = %s, new position = %s" % (
-            self.current_position, the_position))
-            self.logger.debug("predefinedPositionChanged emitted: %s" % the_position)
-            self.current_position = the_position
-            self.emit('predefinedPositionChanged', (the_position, 0))
+        """
+        Slot receiving the zoom position changed and emitting the signal.
+        Additionally, we could also actuate the backlight intensity here.
+
+        Args:
+            position: Zoom int position
+
+        Returns: None
+
+        """
+        # TODO: Arrives an int, emits string
+        # We should use the position received.
+        position = self.getCurrentPositionName()
+
+        if position != self.current_position:
+            self.logger.debug(
+                "Zoom changed: {} -> {}".format(self.current_position, position))
+            self.current_position = position
+            self.emit('predefinedPositionChanged', position)
             # TODO: blight range is 0-30
             # Links the zoom position with the backlight value
-            #self.chan_blight.setValue(int(self.current_position.split()[0]))
+            # self.chan_blight.setValue(int(self.current_position.split()[0]))
 
     def isReady(self):
-        state = self.getState()
-        #TODO: server state is ALWAYS Unknown
+        """
+        Bliss API.
+
+        Returns: Boolean
+
+        """
+        # state = self.getState()
+        # return state == self.STATE.READY
+        # TODO: Bug in Arinax server: state is ALWAYS UNKNOWN
         return True
-        return state == self.STATE.READY
