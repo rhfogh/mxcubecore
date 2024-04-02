62d8e786f3486c5a12fa4add2fd641410431332c
Antonia Beteva <beteva@esrf.fr>
2024-04-02 08:18:02 +0200

    [XRF] - Path to chooch changed

--- mxcubecore/HardwareObjects/ESRF/ESRFEnergyScan.py
+++ mxcubecore/HardwareObjects/ESRF/ESRFEnergyScan.py
@@ -270,7 +270,7 @@ class ESRFEnergyScan(AbstractEnergyScan, HardwareObject):
         # while waiting fro chooch to work...
         subprocess.call(
             [
-                "/opt/pxsoft/bin/chooch",
+                "/cvmfs/sb.esrf.fr/bin/chooch",
                 "-e",
                 elt,
                 "-a",
