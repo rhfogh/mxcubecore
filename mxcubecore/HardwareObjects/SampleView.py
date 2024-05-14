89107bbd322416c1c01473fa1acf0c935bb78d37
Marcus Oskarsson <oscarsso@esrf.fr>
2024-05-14 13:26:31 +0200

    [CenteredPoint] - Using position name instead of id for index

--- mxcubecore/HardwareObjects/SampleView.py
+++ mxcubecore/HardwareObjects/SampleView.py
@@ -485,7 +485,7 @@ class Point(Shape):
 
     def set_id(self, id_num):
         Shape.set_id(self, id_num)
-        self.cp_list[0].index = self.id
+        self.cp_list[0].index = self.name
 
     def as_dict(self):
         d = Shape.as_dict(self)
@@ -516,7 +516,6 @@ class Line(Shape):
 
     def set_id(self, id_num):
         Shape.set_id(self, id_num)
-        self.cp_list[0].index = self.id
 
     def get_centred_positions(self):
         return [self.start_cpos, self.end_cpos]
@@ -586,7 +585,6 @@ class Grid(Shape):
 
     def set_id(self, id_num):
         Shape.set_id(self, id_num)
-        self.cp_list[0].index = self.id
 
     def set_result(self, result_data):
         self.result = result_data
