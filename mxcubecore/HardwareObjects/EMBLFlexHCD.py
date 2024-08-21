100294e5e69fc6b27b56acf1ac39281642ed1b9f
Marcus Oskarsson <oscarsso@esrf.fr>
2024-08-21 10:09:06 +0200

    [MASSIF1] - Changing phase directly after load if custom change phase script is available

--- mxcubecore/HardwareObjects/EMBLFlexHCD.py
+++ mxcubecore/HardwareObjects/EMBLFlexHCD.py
@@ -561,14 +561,27 @@ class EMBLFlexHCD(SampleChanger):
                     )
                 )
 
-                if loaded_sample == (
+                _use_custom = HWR.beamline.diffractometer.get_property(
+                    "use_custom_phase_script", False
+                )
+
+                if _use_custom and loaded_sample == (
                     sample.get_cell_no(),
                     sample.get_basket_no(),
                     sample.get_vial_no(),
                 ):
+
+                    logging.getLogger("user_level_log").info(
+                        "Sample is Loaded from EMBLFlexHCD.py "
+                    )
+                    HWR.beamline.diffractometer.wait_ready(100)
+                    HWR.beamline.diffractometer.run_script("ChangePhase_centring")
+                    HWR.beamline.diffractometer.run_script(
+                        "sample_centering", wait=False
+                    )
                     break
 
-                gevent.sleep(2)
+                gevent.sleep(1)
 
         with gevent.Timeout(600, RuntimeError(err_msg)):
             while True:
