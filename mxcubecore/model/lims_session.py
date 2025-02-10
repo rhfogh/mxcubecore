b346b2346622024d7617a8efcf78afd887666889
rhfogh <rhfogh@globalphasing.com>
2025-02-10 11:52:25 +0000

    Fixed bug (for Python3.8) in type annotations

--- mxcubecore/model/lims_session.py
+++ mxcubecore/model/lims_session.py
@@ -5,6 +5,7 @@ from datetime import (
 from typing import (
     List,
     Optional,
+    Dict,
 )
 
 from pydantic.v1 import (
@@ -77,4 +78,4 @@ class LimsUser(BaseModel):
 class LimsSessionManager(BaseModel):
     active_session: Optional[Session] = None
     sessions: Optional[List[Session]] = []
-    users: Optional[dict[str, LimsUser]] = {}
+    users: Optional[Dict[str, LimsUser]] = {}
